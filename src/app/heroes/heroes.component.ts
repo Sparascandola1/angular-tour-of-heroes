786a9e811ee39a159823297e77eea94331aa054c
Salvatore <sparascandola1@gmail.com>
2021-10-12 08:45:45 -0700

    select hero feature

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,12 +8,15 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
+  heroes = HEROES;
 
-  heroes = HEROES; 
+  selectedHero?: Hero;
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
-
-
